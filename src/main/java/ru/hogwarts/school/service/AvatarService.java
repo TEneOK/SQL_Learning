7afb6de15f601a8393592276d908cbd9b5c8f861
shade <rgg93455@gmail.com>
2026-01-08 17:43:16 +0300

    Query

---
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -0,0 +1,42 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.AvatarRepository;
+import ru.hogwarts.school.repository.StudentRepository;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Service
+@Transactional
+public class AvatarService {
+
+    @Value("${path.to.avatars.folder:./avatars}")
+    private String avatarsDir;
+
+    private final StudentRepository studentRepository;
+    private final AvatarRepository avatarRepository;
+
+    public AvatarService(StudentRepository studentRepository,
+                         AvatarRepository avatarRepository) {
+        this.studentRepository = studentRepository;
+        this.avatarRepository = avatarRepository;
+    }
+
+    public Page<Avatar> getAllAvatarsWithPagination(int pageNumber, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        return avatarRepository.findAll(pageable);
+    }
+}
